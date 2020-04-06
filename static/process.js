2de065c8294fe64d1f3a465c6c7798c22e71fb5c
Ted <43794751+TeddyChene@users.noreply.github.com>
2020-04-05 16:54:06 -1000

    simplification
    ajout des variables état, état-initial et indice dans les balises <div>

--- static/process.js
+++ static/process.js
@@ -1,23 +1,29 @@
 $(document).ready(function(){
-    
+
     //Récupère la variable depart passée par python à l'initialisation de la page
-    var state = $("#inittaquin").val()
+    var state = $("#taquin").attr("etat-initial")
     console.log("départ " + state);
     //Fonction qui met à jour l'état de la grille représentée par la chaîne des numéro
     function update(state, num){
-        
+
         //Echange la position du 1 et du 9 dans la chaîne qui représente la grille
         //console.log(state.replace(/[19]/g, function($1) { return $1 === '1' ? '9' : '1' }));
         var regex = new RegExp("[" + num + "0]", "g");
         // Cette ligne construit l'expression régulière /[num9]/g pour num variant de 1 à 8
-        return state.replace(regex, function($1) { return $1 === num ? '0' : num });
+        etat = state.replace(regex, function($1) { return $1 === num ? '0' : num });
+        // Mise à jour de l'état du taquin dans la balise div
+        $("#taquin").attr("etat",etat);
+        // Récupération et mise à jour du compteur de coup
+        compteur = parseInt($("#taquin").attr("compteur")) + 1;
+        $("#taquin").attr("compteur",compteur.toString());
+        return etat
     };
 
-    //Fonction qui détermine les coordonnées du coin haut gauche d'une tuile  
+    //Fonction qui détermine les coordonnées du coin haut gauche d'une tuile
     function logposition(element){
         console.log(element.attr('id') + " top " + element.position().top + " left "+ element.position().left);
     };
-    
+
     //Fonction qui renvoie les paramètres de direction pour l'animation d'une tuile
     function getdir(chaine){
         var param=""
@@ -35,7 +41,7 @@ $(document).ready(function(){
         }
         return param
     };
-    
+
     //Fonction qui renvoie les paramètres de direction pour l'animation de la tuile vide
     function oppositedir(chaine){
         var param=""
@@ -53,50 +59,50 @@ $(document).ready(function(){
         }
         return param
     };
-    
+
     //Fonction qui échange un élément avec la tuile vide
     function echange(element, direction, duree){
         var TL = gsap.timeline();
         TL
             .to(element, duree, getdir(direction), "+=0.5")
-            .to($("#piece_0"), duree, oppositedir(direction), "-=0.5")  
-    
+            .to($("#piece_0"), duree, oppositedir(direction), "-=0.5")
+
         //console.log("after move " + element.attr('id') + " top " + element.position().top + " left "+ element.position().left);
-        
-        //Equivalent dans l'ancienne version à 
+
+        //Equivalent dans l'ancienne version à
         // gsap.to(element, {duration: 1, x: "-=200", onComplete:function(){
         //            console.log("after move " + element.attr('id') + " top " + element.position().top + " left "+ element.position().left);
-        //            }   
+        //            }
     };
-    
+
     //On écoute les click sur les tuiles (classe piece) et on détermine si l'élément est clickable
     $('div.piece').click(function(){
         //Récupère l'id de la pièce sur laquelle on clique
         console.log("On vient de clicker sur "+ $(this).attr('id'))
         //Récupération du numéro de la piece
-        var numero = $(this).attr('id').charAt($(this).attr('id').length-1);
-        
+        var numero = $(this).attr('indice');
+
         //logposition($(this));
-        //logposition($('#piece_0'));       
+        //logposition($('#piece_0'));
         //Récupère les coordonnées du coin haut gauche de la pièce
         var pos = $(this).position();
-        
+
         var pos_top=pos.top;
         var pos_left=pos.left;
         //console.log("case cliquée à top "+ pos_top + " case cliquée à left "+ pos_left);
-        
-        //Récupère les coordonnées du coin haut gauche de la tuile vide        
+
+        //Récupère les coordonnées du coin haut gauche de la tuile vide
         //var tuilevidetop = ajuste_top($("#piece_9"));
         var tuilevidetop = $("#piece_0").position().top;
         var tuilevideleft = $("#piece_0").position().left;
         //console.log("case vide top "+ tuilevidetop + " case vide left "+ tuilevideleft);
-        
+
         //Calcul de distance par rapport à la tuile vide
         var x=(tuilevideleft-pos_left);
         var y=(tuilevidetop-pos_top);
         var dist=Math.sqrt(x*x+y*y);
         //console.log("dist", dist);
-        
+
         //Test sur la distance pour savoir si c'est clickable
         if (dist>Math.sqrt(80000)-1) {
             var clickable = false;
@@ -104,32 +110,33 @@ $(document).ready(function(){
         else {
             var clickable = true;
         }
-        
+
         //Si l'élément est clickable, on l'échange avec la tuile vide
-        if (clickable && dist!==0){
+        if (clickable && dist !== 0){
             console.log("mouvement autorisé");
-            
+
             //On cherche dans quel sens bouger avant de faire l'échange
-            if (pos_left == tuilevideleft && pos_top < tuilevidetop){
-                echange($(this),"B", 0.5);     
-                var tuilevidedir = "H";   
+            if ((pos_left == tuilevideleft) && (pos_top < tuilevidetop)){
+                echange($(this),"B", 0.5);
+                var tuilevidedir = "H";
             }
             else if (pos_top > tuilevidetop){
                 echange($(this),"H", 0.5);
                 var tuilevidedir = "B";
             }
             if (pos_top == tuilevidetop && pos_left < tuilevideleft){
-                echange($(this),"D", 0.5); 
-                var tuilevidedir = "G";       
+                echange($(this),"D", 0.5);
+                var tuilevidedir = "G";
             }
             else if (pos_left > tuilevideleft){
                 echange($(this),"G", 0.5);
                 var tuilevidedir = "D";
             }
             //Mise à jour de la chaîne représentant l'état (équivalent à Etat(untaquin) dans la partie python)
+            //state = update(state, numero);
             state = update(state, numero);
             console.log("update "+ state);
-            
+
             request = {"action": "mymove", "argument2": tuilevidedir};
             send_info('/action', request);
         }
@@ -137,7 +144,7 @@ $(document).ready(function(){
             alert("Vous ne pouvez pas déplacer cette tuile");
         }
         });
-    
+
     //Fonction d'envoi de données au module python
     function send_info(receiver, to_send){
         $.ajax({
@@ -149,12 +156,12 @@ $(document).ready(function(){
             })
             // On exécute cette fonction au retour
             // Pour l'instant, on montre un mélange des tuiles via une animation
-            .done(function(data){               
+            .done(function(data){
                 // produit un dictionnaire à partir des données envoyées par python
-                var returned = JSON.parse(data); 
+                var returned = JSON.parse(data);
                 console.log("return data => clefs " + Object.keys(returned)[0] + " " + Object.keys(returned)[1]);
                 console.log("return data => valeurs " + Object.values(returned)[0] + " " + Object.values(returned)[1]);
-                
+
                 if (Object.values(returned)[0] == "shuffle"){
                     var stringtoprocess = Object.values(returned)[1];
                     //Création d'une timeline pour l'animation
@@ -164,7 +171,7 @@ $(document).ready(function(){
                         shuffleTL
                             .to($("#piece_" + stringtoprocess[2*i]), 0.5, getdir(stringtoprocess[2*i+1]), "+=0.5")
                             .to($("#piece_0"), 0.5, oppositedir(stringtoprocess[2*i+1]), "-=0.5")
-                
+
                     }
                 }
                 if (Object.values(returned)[0] == "mymove"){
@@ -172,15 +179,15 @@ $(document).ready(function(){
                 }
             })
     }
-    
+
     //On écoute le click sur le bouton qui actionne le mélange
     $("#btn_melange").click(function(event){
         event.preventDefault();
         button_id = $(this).attr('id');
-        
+
         if (button_id == "btn_melange"){
                 request = {"action":"shuffle", "argument2":state};
-                send_info('/action', request);         
+                send_info('/action', request);
         }
     });
 
