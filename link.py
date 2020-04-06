9344540d8e42131e583d96503b7dceb7ad658315
chiudidier <51762257+chiudidier@users.noreply.github.com>
2020-04-05 18:58:47 -1000

    Update link.py

--- link.py
+++ link.py
@@ -70,6 +70,22 @@ def playtaquin():
     print('Bravo vous avez gagné !')
 
 
+def whereTo(etat1, etat2):
+    pos_zero_etat1 = etat1.index('0')
+    pos_zero_etat2 = etat2.index('0')
+    who = etat2[pos_zero_etat1]
+    
+    if (pos_zero_etat1 % 3 == pos_zero_etat2 % 3) and (pos_zero_etat1 < pos_zero_etat2):
+        where = "H"
+    if (pos_zero_etat1 % 3 == pos_zero_etat2 % 3) and (pos_zero_etat1 > pos_zero_etat2):
+        where = "B"
+    if (pos_zero_etat1 % 3 - pos_zero_etat2 % 3 == 1):
+        where = "D"
+    if (pos_zero_etat1 % 3 - pos_zero_etat2 % 3 == -1):
+        where = "G"
+    
+    return(who+where)
+    
 #==============
 # Phase de jeu
 #==============
@@ -97,7 +113,7 @@ def ANewHope():
     #taquindejeu est initialisé au début à "012345678" par la fonction initializetaquin (voir plus haut)
     #mélange du taquin
     taquindejeu.melanger(25)
-    print("test next", Etat(taquindejeu).val) #contient par exemple "312645780"
+    print("Le taquin au départ", Etat(taquindejeu).val) #contient par exemple "312645780"
     #chaîne à envoyer pour transmettre l'état 
     machaine = Etat(taquindejeu).val
     
@@ -108,7 +124,7 @@ def ReturnOfTheJedi():
     
     # Récupération de l'action demandée par JS (ici une chaine de caractères)
     actionaskedbyJS = request.args.get("action", type = str)
-    argument2fromJS = request.args.get("argument2", type = str)
+    argument2fromJS = request.args.get("argument2", type = str) #argument2fromJS contient l'état (chaîne) du taquin avant le click
     print("\nCe que j'ai reçu de JS", actionaskedbyJS, argument2fromJS)
               
     # Préparation de la réponse à envoyer à JS    
@@ -129,9 +145,37 @@ def ReturnOfTheJedi():
             argument2_tosend = "done"
         
         # Si on a clické sur le boutton dans la page web
-        elif (actionaskedbyJS == "shuffle"):
+        elif (actionaskedbyJS == "giveup"):
+            
+            if taquindejeu.gagnant() == False:
+                # calcul la profondeur minimum de la solution
+                reste = bfs(taquindejeu)
+                chemin = []
+                etatzero = Etat(taquindejeu)
+                dfs(reste,etatzero,0,chemin)
+                
+                # affiche l'aide
+                print(reste, 'mouvements au moins pour terminer. \nSolution =', chemin)
+                
+                # retire de la liste des états suivants le premier pour affichage
+                etat1 = etatzero.val
+                toVictory = ''
+                
+                while(chemin != []):
+                    nextmove = chemin.pop()
+                    nexttaquin = Taquin(nextmove)
+                    #print('meilleur coup suivant :')
+                    #print(nexttaquin)     
+                    #print("step zero", etatzero)
+                    
+                    etat2 = nextmove
+                    print("who really needs to move", whereTo(etat1, etat2))
+                    toVictory += whereTo(etat1, etat2)
+                    etat1 = nextmove
+                print("road to victory", toVictory)
+            
             #la variable renvoyée à JS doit être un string, tuple, response instance ou WSGI callable
-            argument2_tosend = "" #1G4H3D6H7G8G5B2B
+            argument2_tosend = toVictory #1G4H3D6H7G8G5B2B
         else:
             argument2_tosend="NULL"
     else:
