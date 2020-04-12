73056f0f085108a43d3cd0aef40ccb3c10ba5950
chiudidier <51762257+chiudidier@users.noreply.github.com>
2020-04-12 09:47:11 -1000

    Update process.js

--- static/process.js
+++ static/process.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
     
-    //Récupère la variable depart passée par python à l'initialisation de la page
+    // Récupère la variable depart passée par python à l'initialisation de la page
     var state = $("#inittaquin").val()
-    console.log("départ " + state);
+    console.log("Engine started " + state);
     
-    //un booléen pour savoir si une animation est en cours 
+    // un compteur du nombre de déplacements
+    var nbclick = 0;
+    // un booléen pour savoir si une animation est en cours 
     var lock = true;
-    //Fonction qui met à jour l'état de la grille représentée par la chaîne des numéro
+    // Fonction qui met à jour l'état de la grille représentée par la chaîne des numéro
     function update(state, num){
         
-        //Echange la position du 1 et du 9 dans la chaîne qui représente la grille
+        // Echange la position du 1 et du 9 dans la chaîne qui représente la grille
         //console.log(state.replace(/[19]/g, function($1) { return $1 === '1' ? '9' : '1' }));
         var regex = new RegExp("[" + num + "0]", "g");
         // Cette ligne construit l'expression régulière /[num9]/g pour num variant de 1 à 8
         return state.replace(regex, function($1) { return $1 === num ? '0' : num });
     };
 
-    //Fonction qui détermine les coordonnées du coin haut gauche d'une tuile  
+    // Fonction qui détermine les coordonnées du coin haut gauche d'une tuile  
     function logposition(element){
         console.log(element.attr('id') + " top " + element.position().top + " left "+ element.position().left);
     };
     
-    //Fonction qui renvoie les paramètres de direction pour l'animation d'une tuile
+    // Fonction qui renvoie les paramètres de direction pour l'animation d'une tuile
     function getdir(chaine){
         var param=""
         if(chaine=="G"){
@@ -39,7 +41,7 @@ $(document).ready(function(){
         return param
     };
     
-    //Fonction qui renvoie les paramètres de direction pour l'animation de la tuile vide
+    // Fonction qui renvoie les paramètres de direction pour l'animation de la tuile vide
     function oppositedir(chaine){
         var param=""
         if(chaine=="G"){
@@ -57,54 +59,42 @@ $(document).ready(function(){
         return param
     };
     
-    //Fonction qui échange un élément avec la tuile vide
+    // Fonction qui échange un élément avec la tuile vide
     function echange(element, direction, duree){
         var TL = gsap.timeline();
         TL
             .to(element, duree, getdir(direction), "+=0.5")
             .to($("#piece_0"), duree, oppositedir(direction), "-=0.5")  
-    
-        //console.log("after move " + element.attr('id') + " top " + element.position().top + " left "+ element.position().left);
-        
-        //Equivalent dans l'ancienne version à 
-        // gsap.to(element, {duration: 1, x: "-=200", onComplete:function(){
-        //            console.log("after move " + element.attr('id') + " top " + element.position().top + " left "+ element.position().left);
-        //            }   
     };
     
-    
-    
-    var nbclick = 0;
-    
-    //On écoute les click sur les tuiles (classe piece) et on détermine si l'élément est clickable
+    // On écoute les click sur les tuiles (classe piece) et on détermine si l'élément est clickable
     $('div.piece').click(function(){
         
-        //Récupère l'id de la pièce sur laquelle on clique
-        console.log("On vient de clicker sur "+ $(this).attr('id'))
+        // Récupère l'id de la pièce sur laquelle on clique
+        console.log("You just clicked on "+ $(this).attr('id'))
                
-        //Récupération du numéro de la piece
+        // Récupère le numéro de la tuile
         var numero = $(this).attr('id').charAt($(this).attr('id').length-1);
         
-        //Récupère les coordonnées du coin haut gauche de la pièce
+        // Récupère les coordonnées du coin haut gauche de la tuile
         var pos = $(this).position();
         
         var pos_top=pos.top;
         var pos_left=pos.left;
         //console.log("case cliquée à top "+ pos_top + " case cliquée à left "+ pos_left);
         
-        //Récupère les coordonnées du coin haut gauche de la tuile vide        
-        //var tuilevidetop = ajuste_top($("#piece_9"));
+        // Récupère les coordonnées du coin haut gauche de la tuile vide        
         var tuilevidetop = $("#piece_0").position().top;
         var tuilevideleft = $("#piece_0").position().left;
         //console.log("case vide top "+ tuilevidetop + " case vide left "+ tuilevideleft);
         
-        //Calcul de distance par rapport à la tuile vide
+        // Calcul de distance par rapport à la tuile vide
         var x=(tuilevideleft-pos_left);
         var y=(tuilevidetop-pos_top);
         var dist=Math.sqrt(x*x+y*y);
         //console.log("dist", dist);
         
-        //Test sur la distance pour savoir si c'est clickable
+        // Test sur la distance pour savoir si c'est clickable
         if (dist>Math.sqrt(80000)-1) {
             var clickable = false;
         }
@@ -112,16 +102,16 @@ $(document).ready(function(){
             var clickable = true;
         }
         
-        //Si l'élément est clickable, on l'échange avec la tuile vide
+        // Si l'élément est clickable, on l'échange avec la tuile vide
         if (clickable && dist!==0){
-            console.log("mouvement autorisé");
+            console.log("This is a legal move");
             
-            //Mise à jour du nombre de clicks
+            // Mise à jour du nombre de déplacements
             nbclick += 1;
             //console.log("nb de clicks", nbclick)
             $("#compteur").text("Déplacements effectués : " + nbclick);
             
-            //On cherche dans quel sens bouger avant de faire l'échange
+            // On cherche dans quel sens bouger avant de faire l'échange
             if (pos_left == tuilevideleft && pos_top < tuilevidetop){
                 echange($(this),"B", 0.5);     
                 var tuilevidedir = "H";   
@@ -140,17 +130,17 @@ $(document).ready(function(){
             }
             //Mise à jour de la chaîne représentant l'état (équivalent à Etat(untaquin) dans la partie python)
             state = update(state, numero);
-            console.log("update "+ state);
+            console.log("Update "+ state);
             
             request = {"action": "mymove", "argument2": tuilevidedir};
             send_info('/action', request);
         }
         else {
-            alert("Vous ne pouvez pas déplacer cette tuile");
+            alert("This is not the tile you're looking for");
         }
         });
     
-    //Fonction d'envoi de données au module python
+    // Fonction d'envoi de données au module python
     function send_info(receiver, to_send){
         $.ajax({
             type : 'GET',
@@ -160,12 +150,16 @@ $(document).ready(function(){
             error : function(){console.log("BSoD : Erreur fatale pendant la demande d'action " + Object.values(to_send)[0]);}
             })
             // On exécute cette fonction au retour
-            // Pour l'instant, on montre un mélange des tuiles via une animation
+            // Plusieurs actions sont possibles au retour
+            // Soit on affiche de l'aide
+            // Soit on échange la position d'une tuile avec la case vide
+            // Soit on anime pour accéder à l'état gagnant
             .done(function(data){               
-                // produit un dictionnaire à partir des données envoyées par python
+                // Produit un dictionnaire à partir des données envoyées par python
                 var returned = JSON.parse(data); 
-                console.log("return data => clefs " + Object.keys(returned)[0] + " " + Object.keys(returned)[1]);
-                console.log("return data => valeurs " + Object.values(returned)[0] + " " + Object.values(returned)[1]);
+                console.log("Data received", Object(returned));
+                //console.log("return data => clefs " + Object.keys(returned)[0] + " " + Object.keys(returned)[1]);
+                //console.log("return data => valeurs " + Object.values(returned)[0] + " " + Object.values(returned)[1]);
                 
                 if (Object.values(returned)[0] == "help me Obi-wan Kenobi, you're my only hope"){
                     var nextmove = ""
@@ -189,16 +183,16 @@ $(document).ready(function(){
                             }
                         }
                     }
-                    alert("Un coup intéressant serait d'arriver à : "+ nextmove)
+                    alert("I have a good (not a bad one) feeling about this : "+ nextmove)
                 
                     }
                 // Sinon c'est qu'il faut faire l'animation d'un déplacement    
                 else if (Object.values(returned)[0] == "mymove"){
-                    console.log("swap of pieces done")
+                    //console.log("swap of pieces done")
                     
                     setTimeout(function () {
                         if (state == '012345678') {
-                            alert('Congratulations on solving the puzzle !');
+                            alert("You've just restored balance to the Force and brought peace to the Galaxy");
                         }
                     }, 1500);
                     
@@ -227,23 +221,20 @@ $(document).ready(function(){
         
         if (button_id == 'btn_helpme'){
             request = {"action":"help me Obi-wan Kenobi, you're my only hope", "argument2":state}
-            console.log("infos", request);
-            send_info('/action', request);
         }
         
         else {
             //console.log("id du bouton", button_id);
             searchmethod = button_id.slice(button_id.length - 7);
-            request = {"action":searchmethod, "argument2":state};
-            console.log("infos", request);
-            send_info('/action', request);
-    
+            request = {"action":searchmethod, "argument2":state};   
         }
+        console.log("Data sent", request);
+        send_info('/action', request);
         
     });
     
     
-    //Bouton pour remettre à 0 le compteur de déplacements
+    // Bouton pour remettre à 0 le compteur de déplacements
     $("#reset").click(function(event) {
             $("#compteur").text("Déplacements effectués : 0");
             nbclick = 0;
